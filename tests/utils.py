335f5ec1098441517cb44fe01bb268646ce8f335
mila <125148912+MilaaPython@users.noreply.github.com>
2023-03-24 14:20:42 +0300

    Update utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -1,49 +1,177 @@
-import requests
-from datetime import datetime
+import json
+import pytest
+
+import utils.utils
+import main
 
 
-def get_data(url):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.json(), "INFO: Данные получены успешно!"
-        return None, f"WARNING: Статус ответа {response.status_code}"
-    except requests.exceptions.ConnectionError:
-        return None, "ERROR: requests.exceptions.ConnectionError"
+@pytest.fixture
+def test_data():
+    with open(main.data_file, 'rt', encoding='utf-8') as data_file:
+        return json.loads("".join(data_file.readlines()))
 
 
-def get_filtered_data(data, filtered_empty_from=False):
-    data = [x for x in data if "state" in x and x["state"] == 'EXECUTED']
-    if filtered_empty_from:
-        data = [x for x in data if "from" in x]
-    return data
+@pytest.fixture
+def test_data_ok():
+    result = []
+    with open(main.data_file, 'rt', encoding='utf-8') as data_file:
+        for item in json.loads("".join(data_file.readlines())):
+            if item:
+                result.append(item)
+    return result
 
 
-def get_last_data(data, count_last_values):
-    data = sorted(data, key=lambda x: x['date'], reverse=True)
-    return data[:count_last_values]
+@pytest.fixture
+def test_6_items():
+    return [
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        },
+        {
+            "id": 41428829,
+            "state": "EXECUTED",
+            "date": "2019-07-03T18:35:29.512364",
+            "operationAmount": {
+                "amount": "8221.37",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "MasterCard 7158300734726758",
+            "to": "Счет 35383033474447895560"
+        },
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        },
+        {
+            "id": 587085106,
+            "state": "EXECUTED",
+            "date": "2018-03-23T10:45:06.972075",
+            "operationAmount": {
+                "amount": "48223.05",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Открытие вклада",
+            "to": "Счет 41421565395219882431"
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        },
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160"
+        },
+    ]
 
 
-def get_formatted_data(data):
-    formatted_data = []
-    for row in data:
-       date = datetime.strptime(row["date"], "%Y-%m-%dT%H:%M:%S.%f").strftime("%d.%m.%Y")
-       description = row["description"]
+def test_get_operations(test_data):
+    assert utils.utils.get_operations(main.data_file) != test_data
 
 
-       sender = row["from"].split()
-       sender_bill = sender.pop(-1)
-       sender_bill = f"{sender_bill[:4]} {sender_bill[4:6]}** **** {sender_bill[-4:]}"
-       sender_info = " ".join(sender)
+def test_filter_by_state(test_data_ok):
+    for item in utils.utils.filter_by_state(test_data_ok, 'EXECUTED'):
+        assert item['state'] == 'EXECUTED'
 
-    
-       recipient = f"**{row['to'][-4:]}"
-       amount = f'{row["operationAmount"]["amount"]} {row["operationAmount"]["currency"]["name"]}'
-       formatted_data.append(f"""\
-{date} {description}
-{sender_info} {sender_bill} -> Счет {recipient}
-{amount}
-""")
-    return formatted_data
+    for item in utils.utils.filter_by_state(test_data_ok, 'CANCELED'):
+        assert item['state'] == 'CANCELED'
+
+    assert utils.utils.filter_by_state(test_data_ok, 'TEST') == []
+
+
+def test_templ_operation(test_6_items):
+    data, descr, source, destin, amount, currency = utils.utils.templ_operation(test_6_items[0])
+    assert data == '26.08.2019'
+    assert descr == "Перевод организации"
+    assert source == "Maestro 1596 83** **** 5199"
+    assert destin == "Счет **9589"
+    assert amount == "31957.58"
+    assert currency == "руб."
+    data, descr, source, destin, amount, currency = utils.utils.templ_operation(test_6_items[2])
+    assert data == '30.06.2018'
+    assert descr == "Перевод организации"
+    assert source == "Счет **6952"
+    assert destin == "Счет **6702"
+    assert amount == "9824.07"
+    assert currency == "USD"
+
+
+def test_format_date():
+    assert utils.utils.format_date("2019-08-26T10:50:58.294041") == "26.08.2019"
+    assert utils.utils.format_date("2019-07-03T18:35:29.512364") == "03.07.2019"
+    assert utils.utils.format_date("2018-06-30T02:08:58.425572") == "30.06.2018"
+
+
+def test_add_mask():
+    assert utils.utils.add_mask("MasterCard 7158300734726758") == "MasterCard 7158 30** **** 6758"
+    assert utils.utils.add_mask("Счет 64686473678894779589") == "Счет **9589"
+    assert utils.utils.add_mask("Счет 646864736788") == 'Счет ???_номер_карты,_счёта_???'
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+
+
 
 
